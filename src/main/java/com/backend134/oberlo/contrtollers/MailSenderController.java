8b8e2e71b564223393a5b83784d7e4bf46e64118
gonchaaa <qonceabaasova@gmail.com>
2025-09-10 20:11:24 +0400

    Mail is succesfully

--- src/main/java/com/backend134/oberlo/contrtollers/MailSenderController.java
+++ src/main/java/com/backend134/oberlo/contrtollers/MailSenderController.java
@@ -5,6 +5,7 @@ import com.backend134.oberlo.repositories.UserRepository;
 import com.backend134.oberlo.services.impl.MailServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +18,18 @@ public class MailSenderController {
     private final MailServiceImpl mailService;
 
     @PostMapping()
-    public String sendMail(Users users) {
-        userRepository.save(users);
-        mailService.sendMail(users);
+    public String sendMail(@RequestBody Users users) {
+        System.out.println("metos ise dusdu");
+        System.out.println(users.getEmail());
+        Users user = new Users();
+        user.setEmail(users.getEmail());
+        user.setName(users.getName());
+        user.setPhoneNumber(users.getPhoneNumber());
+        user.setDescription(users.getDescription());
+        userRepository.save(user);
+        System.out.println(user.getEmail());
+        System.out.println(user.getName());
+        mailService.sendMail(user);
         return "Mail sent successfully";
     }
 }
